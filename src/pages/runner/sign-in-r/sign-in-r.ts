29c21f2a6b41f0d948e56a55223be875c9977bf0
Harris Ilham <harrisilham0046@gmail.com>
2017-11-03 20:12:24 +0800

    +login functionality for all

--- src/pages/runner/sign-in-r/sign-in-r.ts
+++ src/pages/runner/sign-in-r/sign-in-r.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import firebase from 'firebase';
 
 import { HomeRPage } from '../home-r/home-r';
 import { FrontPage } from '../../front/front'
@@ -19,11 +20,17 @@ import { FrontPage } from '../../front/front'
 export class SignInRPage {
   email: any;
   password: any;
+  pathString: any;
+
+  public Email= {};
+  public Password= {};
+
+  emailRef: firebase.database.Reference;
 
   front:any = FrontPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    
+
   }
 
   ionViewDidLoad() {
@@ -31,13 +38,32 @@ export class SignInRPage {
   }
 
   itemTapped() {
+    this.email= (<HTMLInputElement>document.getElementById('emailR')).value;
+    this.pathString = `/runnerStorage/`+ this.email+ `/` ;
 
-    this.email= (<HTMLInputElement>document.getElementById("emailR")).value;
+    //runner data
+    this.emailRef= firebase.database().ref(this.pathString+'email/');
+    this.emailRef.on('value', dataSnapshot => {
+      this.Email = dataSnapshot.val();
+      })
+
+
+    const passwordRef: firebase.database.Reference = firebase.database().ref(this.pathString+'password/');
+    passwordRef.on('value', dataSnapshot => {
+      this.Password = dataSnapshot.val();
+    })
+
     this.password= (<HTMLInputElement>document.getElementById('passwordR')).value;
 
-    if(this.email=="runner" && this.password=="runner123"){
-      this.navCtrl.setRoot(HomeRPage);
+    if(this.Email){
+      if(this.password==this.Password){
+        this.navCtrl.setRoot(HomeRPage);
+      }
+      else{
+        this.navCtrl.setRoot(SignInRPage);
+      }
     }
+
     else{
       this.navCtrl.setRoot(SignInRPage);
     }
--- src/pages/user/sign-in-u/sign-in-u.ts
+++ src/pages/user/sign-in-u/sign-in-u.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import firebase from 'firebase';
 import { HomeUPage } from '../home-u/home-u';
 import { HomeAPage } from '../../admin/home-a/home-a';
 import { SignUpUPage } from '../sign-up-u/sign-up-u';
@@ -18,6 +19,13 @@ import { SignUpUPage } from '../sign-up-u/sign-up-u';
 export class SignInUPage {
   email:any;
   password: any;
+  pathString: any;
+  pathStringA: any;
+
+  public Email= {};
+  public Password= {};
+  public EmailA= {};
+  public PasswordA= {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
@@ -28,16 +36,48 @@ export class SignInUPage {
   }
 
   itemTapped() {
+    this.email= (<HTMLInputElement>document.getElementById('emailU')).value;
+    this.pathString = `/userStorage/`+ this.email+ `/` ;
+    this.pathStringA= `/adminStorage/`+ this.email+ `/` ;
 
-    this.email= (<HTMLInputElement>document.getElementById("emailU")).value;
+    //user data
+    const emailRef: firebase.database.Reference = firebase.database().ref(this.pathString+'email/');
+    emailRef.on('value', dataSnapshot => {
+      this.Email = dataSnapshot.val();
+    })
+    const passwordRef: firebase.database.Reference = firebase.database().ref(this.pathString+'password/');
+    passwordRef.on('value', dataSnapshot => {
+      this.Password = dataSnapshot.val();
+    })
+
+    //admin data
+    const emailRefA: firebase.database.Reference = firebase.database().ref(this.pathStringA+'email/');
+    emailRefA.on('value', dataSnapshot => {
+      this.EmailA = dataSnapshot.val();
+    })
+    const passwordRefA: firebase.database.Reference = firebase.database().ref(this.pathStringA+'password/');
+    passwordRefA.on('value', dataSnapshot => {
+      this.PasswordA = dataSnapshot.val();
+    })
+
+
     this.password= (<HTMLInputElement>document.getElementById('passwordU')).value;
 
-    if(this.email=="admin" && this.password=="admin123"){
-      this.navCtrl.setRoot(HomeAPage);
+    if(this.Email){
+      if(this.Password==this.password){
+        this.navCtrl.setRoot(HomeUPage);
+      }
+      else{
+        this.navCtrl.setRoot(SignInUPage);
+      }
     }
-
-    else if(this.email=="user" && this.password=="user123"){
-      this.navCtrl.setRoot(HomeUPage);
+    else if(this.EmailA){
+      if(this.PasswordA==this.password){
+        this.navCtrl.setRoot(HomeAPage);
+      }
+      else{
+        this.navCtrl.setRoot(SignInUPage);
+      }
     }
     else{
       this.navCtrl.setRoot(SignInUPage);
