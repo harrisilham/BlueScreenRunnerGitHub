4e7eeb182f2d75748f86421969e8de48340210d3
Harris Ilham <harrisilham0046@gmail.com>
2017-11-11 03:39:21 +0800

    home-a,+runner list

--- src/pages/admin/home-a/home-a.ts
+++ src/pages/admin/home-a/home-a.ts
@@ -12,37 +12,74 @@ import { ViewrunnerAPage } from '../viewrunner-a/viewrunner-a';
   templateUrl: 'home-a.html'
 })
 export class HomeAPage {
+  runner: Array<{email: string, fullName: string, ic: number, password: string, phoneNum: number, username: string}>;
 
-  username:any;
-  email:any;
+  size: number;
+
   pathString: any;
+  emailString: any;
 
-  usernameRef: firebase.database.Reference;
-  emailRef: firebase.database.Reference;
+  pathRef: any;
+  emailRef: any;
+  fullNameRef: any;
+  icRef: any;
+  passwordRef: any;
+  phoneNumRef: any;
+
+  runnerNode: Array<{email: String, fullName: String, ic: number, password: String, phoneNum: number, username: String}>;
+  public runnerList=[];
+  test: any;
+  testEmail: any;
+
+  public runnerNode2=[]; //tk gunaaa
+  public email=[];
+  public fullName=[];
+  public ic=[];
+  public password=[];
+  public phoneNum=[];
+  public username=[];
 
   public Username= {};
   public Email= {};
 
-  viewListRunner(){
-    this.pathString= firebase.database().ref("runnerStorage").orderByKey();
-    this.pathString.once("value")
-    .then(function(snapshot) {
-      snapshot.forEach(function(childSnapshot) {
-        // key will be "ada" the first time and "alan" the second time
-        var key = childSnapshot.key;
-        // childData will be the actual contents of the child
-        var childData = childSnapshot.val();
-      });
-    });
-/*
-      this.pathString = `/runnerStorage/`+ this.username+ `/` ;
-
-      this.usernameRef= firebase.database().ref(this.pathString+'email/');
-      this.usernameRef.on('value', dataSnapshot => { this.Username = dataSnapshot.val();})
-    }*/
-  }
   constructor(public navCtrl: NavController, public events: Events) {
     events.publish('user:entered');
+
+    this.pathString= `/runnerStorage/` ;
+    this.pathRef= firebase.database().ref(this.pathString);
+
+    //get all data
+    this.pathRef.once('value', snapshot => {
+      var index=0;
+      snapshot.forEach(childSnapshot => {
+
+        this.email[index]=  childSnapshot.child("/email/").val();
+        this.fullName[index]=  childSnapshot.child("/fullName/").val();
+        this.ic[index]=  childSnapshot.child("/ic/").val();
+        this.password[index]=  childSnapshot.child("/password/").val();
+        this.phoneNum[index]=  childSnapshot.child("/phoneNum/").val();
+        this.username[index]=  childSnapshot.child("/username/").val();
+
+        /*document.writeln(this.email[index]+ "<br>"+ this.fullName[index]+ "<br>"+ this.ic[index]+ "<br>"+ this.password[index]+ "<br>"+ this.phoneNum[index]+ "<br>"+ this.username[index]+ "<br><br>");*/
+
+        //create table
+        var table: HTMLTableElement= <HTMLTableElement>document.getElementById('listRunner');
+        var row: HTMLTableRowElement = <HTMLTableRowElement>table.insertRow(-1);
+        var cell1: HTMLTableCellElement=<HTMLTableCellElement>row.insertCell(0);
+        var cell2: HTMLTableCellElement=<HTMLTableCellElement>row.insertCell(1);
+        var cell3: HTMLTableCellElement=<HTMLTableCellElement>row.insertCell(2);
+        var cell4: HTMLTableCellElement=<HTMLTableCellElement>row.insertCell(3);
+
+        cell1.innerHTML= index.toString();
+        cell2.innerHTML= this.username[index];
+        cell3.innerHTML= "<button><ion-icon name='create'>edit</ion-icon> </button>";
+        cell4.innerHTML= "<button><ion-icon name='trash'>delete</ion-icon> </button>";
+
+        cell1.style.border=cell2.style.border=cell3.style.border=cell4.style.border= "1px solid ";
+        index++;
+      });
+    });
+
   }
 
   viewButton(event, item){
@@ -59,5 +96,9 @@ export class HomeAPage {
 
   deleteButton(event, item){
     this.navCtrl.push(DeleterunnerAPage);
+  }
+
+  public incrementSize(){
+    this.size++;
   }
 }
