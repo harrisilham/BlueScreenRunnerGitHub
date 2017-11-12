bdf8fe2e0f4d2ff8460ba9161d64f18d8bd3dcab
Harris Ilham <harrisilham0046@gmail.com>
2017-11-13 04:41:35 +0800

    test refresher home u

--- src/pages/user/home-u/home-u.ts
+++ src/pages/user/home-u/home-u.ts
@@ -47,4 +47,13 @@ export class HomeUPage {
     });
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
 }
